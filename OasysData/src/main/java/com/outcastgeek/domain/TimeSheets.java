f69b84fd967ce389617bba918174186d8693d5bb
outcastgeek <devnull@localhost>
2012-08-28 08:45:38 -0500

    Implementing Admin Area as an extension!!!!

--- OasysData/src/main/java/com/outcastgeek/domain/TimeSheets.java
+++ OasysData/src/main/java/com/outcastgeek/domain/TimeSheets.java
@@ -25,79 +25,46 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Configurable
 @Entity
-@Table(schema = "public", name = "time_sheets")
+@Table(schema = "public",name = "time_sheets")
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(versionField = "", table = "time_sheets", schema = "public")
 @RooDbManaged(automaticallyDelete = true)
 public class TimeSheets {
 
-    @PersistenceContext
+	@PersistenceContext
     transient EntityManager entityManager;
 
-    @Column(name = "employee_id")
-    private Integer employeeId;
-
-    @Column(name = "payroll_cycle_id")
-    private Integer payrollCycleId;
-
-    @Column(name = "created_at")
-    @NotNull
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "M-")
-    private Date createdAt;
-
-    @Column(name = "updated_at")
-    @NotNull
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "M-")
-    private Date updatedAt;
-
-    @Column(name = "start_date")
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(style = "M-")
-    private Date startDate;
-
-    @Column(name = "end_date")
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(style = "M-")
-    private Date endDate;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Integer id;
-
-    public static final EntityManager entityManager() {
+	public static final EntityManager entityManager() {
         EntityManager em = new TimeSheets().entityManager;
         if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
         return em;
     }
 
-    public static long countTimeSheetses() {
+	public static long countTimeSheetses() {
         return entityManager().createQuery("SELECT COUNT(o) FROM TimeSheets o", Long.class).getSingleResult();
     }
 
-    public static List<com.outcastgeek.domain.TimeSheets> findAllTimeSheetses() {
+	public static List<TimeSheets> findAllTimeSheetses() {
         return entityManager().createQuery("SELECT o FROM TimeSheets o", TimeSheets.class).getResultList();
     }
 
-    public static com.outcastgeek.domain.TimeSheets findTimeSheets(Integer id) {
+	public static TimeSheets findTimeSheets(Integer id) {
         if (id == null) return null;
         return entityManager().find(TimeSheets.class, id);
     }
 
-    public static List<com.outcastgeek.domain.TimeSheets> findTimeSheetsEntries(int firstResult, int maxResults) {
+	public static List<TimeSheets> findTimeSheetsEntries(int firstResult, int maxResults) {
         return entityManager().createQuery("SELECT o FROM TimeSheets o", TimeSheets.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
     }
 
-    @Transactional
+	@Transactional
     public void persist() {
         if (this.entityManager == null) this.entityManager = entityManager();
         this.entityManager.persist(this);
     }
 
-    @Transactional
+	@Transactional
     public void remove() {
         if (this.entityManager == null) this.entityManager = entityManager();
         if (this.entityManager.contains(this)) {
@@ -108,83 +75,116 @@ public class TimeSheets {
         }
     }
 
-    @Transactional
+	@Transactional
     public void flush() {
         if (this.entityManager == null) this.entityManager = entityManager();
         this.entityManager.flush();
     }
 
-    @Transactional
+	@Transactional
     public void clear() {
         if (this.entityManager == null) this.entityManager = entityManager();
         this.entityManager.clear();
     }
 
-    @Transactional
-    public com.outcastgeek.domain.TimeSheets merge() {
+	@Transactional
+    public TimeSheets merge() {
         if (this.entityManager == null) this.entityManager = entityManager();
         TimeSheets merged = this.entityManager.merge(this);
         this.entityManager.flush();
         return merged;
     }
 
-    public Integer getEmployeeId() {
+	@Column(name = "employee_id")
+    private Integer employeeId;
+
+	@Column(name = "payroll_cycle_id")
+    private Integer payrollCycleId;
+
+	@Column(name = "created_at")
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date createdAt;
+
+	@Column(name = "updated_at")
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date updatedAt;
+
+	@Column(name = "start_date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(style = "M-")
+    private Date startDate;
+
+	@Column(name = "end_date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(style = "M-")
+    private Date endDate;
+
+	public Integer getEmployeeId() {
         return employeeId;
     }
 
-    public void setEmployeeId(Integer employeeId) {
+	public void setEmployeeId(Integer employeeId) {
         this.employeeId = employeeId;
     }
 
-    public Integer getPayrollCycleId() {
+	public Integer getPayrollCycleId() {
         return payrollCycleId;
     }
 
-    public void setPayrollCycleId(Integer payrollCycleId) {
+	public void setPayrollCycleId(Integer payrollCycleId) {
         this.payrollCycleId = payrollCycleId;
     }
 
-    public Date getCreatedAt() {
+	public Date getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
+	public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
 
-    public Date getUpdatedAt() {
+	public Date getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
+	public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
 
-    public Date getStartDate() {
+	public Date getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(Date startDate) {
+	public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
-    public Date getEndDate() {
+	public Date getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(Date endDate) {
+	public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
 
-    public Integer getId() {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+	public Integer getId() {
         return this.id;
     }
 
-    public void setId(Integer id) {
+	public void setId(Integer id) {
         this.id = id;
     }
 
-    public String toString() {
+	public String toString() {
         return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 }
---
+++ OasysData/src/main/java/com/outcastgeek/model/security/Principal.java
@@ -0,0 +1,156 @@
+package com.outcastgeek.model.security;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configurable
+@Entity
+@Table(name = "security_principals")
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(table = "security_principals")
+public class Principal {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String password;
+
+    private Boolean enabled;
+
+	@PersistenceContext
+    transient EntityManager entityManager;
+
+	public static final EntityManager entityManager() {
+        EntityManager em = new Principal().entityManager;
+        if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
+        return em;
+    }
+
+	public static long countPrincipals() {
+        return entityManager().createQuery("SELECT COUNT(o) FROM Principal o", Long.class).getSingleResult();
+    }
+
+	public static List<Principal> findAllPrincipals() {
+        return entityManager().createQuery("SELECT o FROM Principal o", Principal.class).getResultList();
+    }
+
+	public static Principal findPrincipal(Long id) {
+        if (id == null) return null;
+        return entityManager().find(Principal.class, id);
+    }
+
+	public static List<Principal> findPrincipalEntries(int firstResult, int maxResults) {
+        return entityManager().createQuery("SELECT o FROM Principal o", Principal.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+
+	@Transactional
+    public void persist() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.persist(this);
+    }
+
+	@Transactional
+    public void remove() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        if (this.entityManager.contains(this)) {
+            this.entityManager.remove(this);
+        } else {
+            Principal attached = Principal.findPrincipal(this.id);
+            this.entityManager.remove(attached);
+        }
+    }
+
+	@Transactional
+    public void flush() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.flush();
+    }
+
+	@Transactional
+    public void clear() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.clear();
+    }
+
+	@Transactional
+    public Principal merge() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        Principal merged = this.entityManager.merge(this);
+        this.entityManager.flush();
+        return merged;
+    }
+
+	public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+	@Version
+    @Column(name = "version")
+    private Integer version;
+
+	public Long getId() {
+        return this.id;
+    }
+
+	public void setId(Long id) {
+        this.id = id;
+    }
+
+	public Integer getVersion() {
+        return this.version;
+    }
+
+	public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+	public String getUsername() {
+        return this.username;
+    }
+
+	public void setUsername(String username) {
+        this.username = username;
+    }
+
+	public String getPassword() {
+        return this.password;
+    }
+
+	public void setPassword(String password) {
+        this.password = password;
+    }
+
+	public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+	public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
