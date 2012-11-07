362d5083df47e3bff5845f05a9839a8daff32ff0
outcastgeek <devnull@localhost>
2012-11-07 08:51:38 -0600

    Minor refactoring!!!!

--- BackgroundJobs/celeryconfig.py
+++ BackgroundJobs/celeryconfig.py
@@ -3,7 +3,7 @@
 BROKER_URL = 'redis://localhost:6379/0'
 
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ("tasks", )
+CELERY_IMPORTS = ("tasks.tasks", )
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
@@ -17,12 +17,12 @@ from datetime import timedelta
 
 CELERYBEAT_SCHEDULE = {
     'runs-add-every-0.4-seconds': {
-        'task': 'tasks.add',
+        'task': 'tasks.tasks.add',
         'schedule': timedelta(seconds=0.4),
         'args': (16, 16)
     },
     'runs-fib-every-0.4-seconds': {
-        'task': 'tasks.fib',
+        'task': 'tasks.tasks.fib',
         'schedule': timedelta(seconds=0.4),
         'args': (128,)
     },
--- BackgroundJobs/tasks/tasks.py
+++ BackgroundJobs/tasks/tasks.py
@@ -1,4 +1,7 @@
 
+import pyximport; pyximport.install()
+import cython
+
 from celery import Celery
 from celery.contrib import rdb
 
@@ -6,10 +9,15 @@ celery = Celery()
 #celery.config_from_object('celeryconfig')
 
 @celery.task
+@cython.cfunc
+@cython.locals(x=cython.int, y=cython.int)
 def add(x, y):
     return x + y
 
 @celery.task
+@cython.cfunc
+@cython.returns(cython.int)
+@cython.locals(n=cython.int)
 def fib(n): # return Fibonacci series up to n
     result = []
     a, b = 0, 1
