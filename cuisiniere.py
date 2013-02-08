8a30502a5cc72819eeb2cdf049b8d7f6d11c9a14
outcastgeek <devnull@localhost>
2013-02-08 08:11:22 -0600

    Added initial cuisine script!!!!

---
+++ cuisiniere.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+from cuisine import *
+from fabric.api import *
+from fabric.context_managers import *
+from fabric.utils import puts
+from fabric.colors import red, green
+
+env.hosts = ['vagrant@127.0.0.1:2222']
+
+def setup_packages():
+    puts(green('Installing Ubuntu packages'))
+    sudo('apt-get update')
+    package_ensure('python-software-properties')
+    sudo('add-apt-repository ppa:webupd8team/java')
+    sudo('apt-get update')
+    
+    package_ensure('build-essential')
+    package_ensure('nginx')
+    package_ensure('postgresql')
+    package_ensure('postgis')
+    package_ensure('postgresql-contrib')
+    package_ensure('supervisor')
+    package_ensure('git-core')
+    package_ensure('ufw') # may have to install by hand
+    
+    package_ensure('python-pip')
+    package_ensure('python-psycopg2')
+    package_ensure('python-imaging')
+
+    package_ensure('cython')
+    package_ensure('mercurial')
+    package_ensure('tree')
+    package_ensure('oracle-java7-installer')
+    package_ensure('ant')
+    package_ensure('maven')
