482e5bcbd87932f46d2ac66ad2b052528bbbeb2d
outcastgeek <devnull@localhost>
2013-02-10 10:47:13 -0600

    Updated cuisine to install all dependencies!!!!

--- kitchen/cuisiniere.py
+++ kitchen/cuisiniere.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 from cuisine import *
+#from cuisine_postgresql import postgresql_role_ensure, postgresql_database_ensure
 from fabric.api import *
 from fabric.context_managers import *
 from fabric.utils import puts
@@ -8,35 +9,62 @@ from fabric.colors import red, green
 
 env.hosts = ['vagrant@127.0.0.1:2222']
 
+puts(green('Provisionning server...'))
+
 def setup_packages():
+    #Ubuntu
     puts(green('Installing Ubuntu packages'))
     sudo('apt-get update')
     package_ensure('python-software-properties')
-    #sudo('add-apt-repository ppa:webupd8team/java')
-    #sudo('apt-get update')
-    
+    sudo('add-apt-repository --yes ppa:gophers/go')
+    sudo('apt-get update')
     package_ensure('build-essential')
     package_ensure('nginx')
+    package_ensure('python-dev')
     package_ensure('postgresql')
     package_ensure('postgis')
     package_ensure('postgresql-contrib')
-    package_ensure('supervisor')
+    package_ensure('postgresql-server-dev-all')
     package_ensure('git-core')
     package_ensure('ufw') # may have to install by hand
-    
-    package_ensure('python-pip')
-    package_ensure('python-psycopg2')
-    package_ensure('python-imaging')
-
-    package_ensure('cython')
-    package_ensure('mercurial')
     package_ensure('tree')
+    package_ensure('libzmq-dev')
+    
+    #Python
+    puts(green('Installing Python packages'))
+    package_ensure('python-setuptools')
+    package_ensure('python-pip')
+    python_package_install_easy_install('supervisor')
+    python_package_install_easy_install('psycopg2')
+    python_package_install_easy_install('pil')
+    python_package_install_easy_install('cython')
+    python_package_install_easy_install('mercurial')
+    python_package_install_easy_install('pyzmq')
+    python_package_install_easy_install('fabric')
+    python_package_install_easy_install('watchdog')
+    python_package_install_easy_install('requests')
+    
+    #JVM
+    puts(green('Installing JVM packages'))
     package_ensure('openjdk-7-jdk')
     package_ensure('ant')
     package_ensure('maven')
+    sudo('update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java')
+
+    #Other
+    puts(green('Installing additional software'))
+    package_ensure('golang-stable')
+    package_ensure('sbcl')
+    package_ensure('ecl')
 
 def setup_users():
-    puts(green('Creating Ubuntu users'))
-    
-    user_ensure('oasystech')
-    
+    puts(green('Creating Ubuntu users'))   
+    user_ensure(name='oasystech', passwd='OasysTech2013!')
+
+    #def configure_database():
+    #    postgresql_role_ensure('postgres', 'PgSQL2012!', createdb=True)
+
+def bootstrap():
+    puts(green('Boostrapping...'))
+    setup_packages()
+    setup_users()
