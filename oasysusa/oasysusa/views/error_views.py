f2b5e781132dc339604f64452573a8e32103dbaf
outcastgeek <devnull@localhost>
2013-09-19 13:21:59 -0500

    Small design touch!!!!

--- oasysusa/oasysusa/views/error_views.py
+++ oasysusa/oasysusa/views/error_views.py
@@ -10,14 +10,23 @@ from pyramid.view import (
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
-@view_config(context=Exception, renderer='templates/error.jinja2')
+# @view_config(context=Exception, renderer='templates/error.jinja2')
+# def error_view(exc, request):
+#     # If the view has two formal arguments, the first is the context.
+#     # The context is always available as ``request.context`` too.
+#     msg = exc.args[0] if exc.args else ""
+#     log.error("\n\nERROR::::\n%s\n\n" % msg)
+#     request.response_status = '500 Error'
+#     return dict(error='Error', message='Things do break you know ...')
+
+@view_config(context=Exception, renderer='templates/500.jinja2')
 def error_view(exc, request):
     # If the view has two formal arguments, the first is the context.
     # The context is always available as ``request.context`` too.
     msg = exc.args[0] if exc.args else ""
     log.error("\n\nERROR::::\n%s\n\n" % msg)
     request.response_status = '500 Error'
-    return dict(error='Error', message='Things do break you know ...')
+    return dict(error='Error', message='We\'re sorry, but something went wrong.')
 
 @notfound_view_config(renderer='templates/error.jinja2')
 def not_found(request):
