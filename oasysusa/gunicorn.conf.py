cfdfc23a94c53257876bcb7d4d72274b623b4ff0
outcastgeek <devnull@localhost>
2013-10-30 19:18:48 -0500

    Configuring gunicorn for PROD!!!!

---
+++ oasysusa/gunicorn.conf.py
@@ -0,0 +1,24 @@
+__author__ = 'outcastgeek'
+
+import os
+
+def get_workers():
+    procs = os.sysconf('SC_NPROCESSORS_ONLN')
+    if procs > 0:
+        return procs * 2 + 1
+    else:
+        return 3
+
+workers = get_workers()
+#workers = 8
+worker_class = tornado
+
+backlog = 2048
+timeout = 30
+keepalive = 2
+debug = False
+spew = False
+accesslog = /home/oasysusa/oasys_corp/logs/oasysusa.log
+
+
+
