75e1eb010fb8fcf2bd74bfa014002b42891e208c
outcastgeek <devnull@localhost>
2013-11-02 05:07:34 -0500

    Conf update

--- oasysusa/run-prod.py
+++ oasysusa/run-prod.py
@@ -32,7 +32,7 @@ def numCPUs():
         raise RuntimeError("No sysconf detected.")
     return os.sysconf("SC_NPROCESSORS_ONLN")
 
-cpus = numCPUs() * 2 + 1
+cpus = numCPUs() * 4 + 1
 
 from subprocess import call
 
@@ -70,8 +70,7 @@ if __name__ == '__main__':
           print("Running in PROD mode...")
           # In case subcommand is a script in some other programming language:
           print("Starting application...")
-          # exit(call('ulimit -n 4096 && /home/oasysusa/ENV/bin/gunicorn --paster --worker-class=gevent --workers=%d production.ini' % cpus, shell=True))
-          exit(call('ulimit -n 4096 && /home/oasysusa/ENV/bin/gunicorn --paste production.ini', shell=True))
+          exit(call('ulimit -n 16384 && /home/oasysusa/ENV/bin/gunicorn_paster --worker-class=gevent --workers=%d production.ini' % cpus, shell=True))
       elif args['--install']:
           print("Installing application...")
           # In case subcommand is a script in some other programming language:
