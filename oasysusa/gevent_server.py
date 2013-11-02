3c5ad4cc4439e4bf9a90bb3db28f9cfc68b65a24
outcastgeek <devnull@localhost>
2013-11-02 16:50:22 -0500

    Conf update

--- oasysusa/gevent_server.py
+++ oasysusa/gevent_server.py
@@ -6,11 +6,14 @@ from gevent import monkey; monkey.patch_all()
 
 if __name__ == '__main__':
 
-    app = get_app('development.ini')
+    # port = 6543
+    port = 8080
+    # app = get_app('development.ini')
+    app = get_app('production.ini')
 
-    print 'Listening on port http://0.0.0.0:8080 and on port 10843 (flash policy server)'
+    print 'Listening on port http://0.0.0.0:%d and on port 10843 (flash policy server)' % port
 
-    SocketIOServer(('0.0.0.0', 6543), app,
+    SocketIOServer(('0.0.0.0', port), app,
                    resource="socket.io", policy_server=True,
                    policy_listener=('0.0.0.0', 10843)).serve_forever()
 
