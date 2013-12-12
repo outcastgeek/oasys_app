8d4c1e387f7d91887287018f54be070c3132a8a6
outcastgeek <devnull@localhost>
2013-12-11 20:58:59 -0600

    Clientside Refactoring!!!!

--- oasysusa/oasysusa/static/js/form.validation.js
+++ oasysusa/oasysusa/static/js/form.validation.js
@@ -1,4 +1,20 @@
 
+
+oasysUsaApp.directive('ngInitial', function() {
+    return {
+        restrict: 'A',
+        controller: [
+            '$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse) {
+                var getter, setter, val;
+                val = $attrs.ngInitial || $attrs.value;
+                getter = $parse($attrs.ngModel);
+                setter = getter.assign;
+                setter($scope, val);
+            }
+        ]
+    };
+});
+
 oasysUsaApp.controller('FormValidationController', function($scope) {
     $scope.master = {};
 
