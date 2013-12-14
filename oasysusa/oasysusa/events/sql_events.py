3563d52c52e63fcaff51badee3558c1070b0a137
outcastgeek <devnull@localhost>
2013-12-13 19:02:30 -0600

    Implementing workers workflow + indexing!!!!

--- oasysusa/oasysusa/events/sql_events.py
+++ oasysusa/oasysusa/events/sql_events.py
@@ -76,13 +76,15 @@ def new_employee_subscriber(event): # TODO: Revisit this!!!!
     employee = event.target
     index_new_employee(employee, settings)
 
+def send_page_for_bulk_index(workers_tcp_address, data):
+    srvc_tell(workers_tcp_address, data)
+
 def handle_index_all_employees(data, settings=None):
     # Get settings
     workers_tcp_address = settings.get('workers_tcp_address')
     # Get employees collection
     employees_collection = Employee.all(Employee.username)
-    # Get lambda
-    get_page = lambda collection, count, i : Page(collection, page=i, item_count=count).items
+
     # Get count
     # count = Employee.session().query(func.count(distinct(Employee.username)))
     count = employees_collection.count()
@@ -90,31 +92,37 @@ def handle_index_all_employees(data, settings=None):
     # Get page count
     page_count = Page(Employee.all(Employee.username), page=1, item_count=count).page_count
     log.debug('\n\nThere are %s pages\n\n', page_count)
-    # Get current page partial
-    get_current_page = partial(get_page, employees_collection, page_count)
-    # Get all employees TODO: Revisit this as it is not very efficient!!!!
-    list_of_list_of_employees = map(get_current_page, xrange(1, page_count + 1))
-    all_employees = itertools.chain(*list_of_list_of_employees)
 
     # Refresh the employee index
     refresh_user_index(settings)
-    # gevent.sleep(4) # Sleep for a while
-    map(lambda employee: srvc_tell(workers_tcp_address, dict(srvc=INDEX_NEW_EMPLOYEE,
-                                                             employee=employee.get_data())), all_employees)
+
+    send_page_for_bulk_index_partial = partial(send_page_for_bulk_index, workers_tcp_address)
+
+    map(lambda page: send_page_for_bulk_index_partial(dict(srvc=INDEX_NEW_EMPLOYEE,
+                                                           count=count,
+                                                           current_page=page)), xrange(1, page_count + 1))
 
     # itertools.chain.from_iterable(
     #     itertools.starmap(lambda i: get_current_page(i),
     #                       itertools.islice(xrange(1, page_count + 1), 1, page_count + 1, 1)))
 
 def handle_index_new_employee_request(data, settings=None):
+    gevent.sleep(1) # Sleep for a while
+    # Get employees collection
+    employees_collection = Employee.all(Employee.username)
+    gevent.sleep(1) # Sleep for a while
+    # Get current page partial
+    count = data.get('count')
+    current_page=data.get('page')
+    page_items = Page(employees_collection, current_page, item_count=count).items
+    employees_data = map(lambda employee: employee.get_data(), page_items)
     try:
-        employee = data.get('employee')
         es = get_es_client(settings)
-        es.index(
+        es.bulk_index(
             EMPLOYEE_INDEX,
             'employee',
-            employee,
-            id=employee.get('id')
+            employees_data,
+            id_field='id'
         )
     except: # catch *all* exceptions
         e = sys.exc_info()[0]
