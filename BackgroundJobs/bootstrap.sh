62b6b51ec849e72fcd10b85914a70f1fbde44bf4
outcastgeek <devnull@localhost>
2013-12-20 13:09:24 -0600

    Code cleanup!!!!

--- BackgroundJobs/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-virtualenv ENV
-ENV/bin/pip install -r requirements.txt || ENV/Scripts/pip install -r requirements.txt
--- BackgroundJobs/runjobs.sh
+++
@@ -1,6 +0,0 @@
-
-..\..\ENV\Scripts\celery.exe worker --config=celeryconfig --loglevel=debug
-
-..\..\ENV\Scripts\celery.exe beat --config=celeryconfig
-
-..\..\ENV\Scripts\celery.exe flower --config=celeryconfig
